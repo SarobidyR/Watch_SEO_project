03e0b762eeff9f4a0b4206a9957776ebe7c7903a
Sky12-design <120624276+SarobidyR@users.noreply.github.com>
2024-07-07 16:35:35 +0300

    script data_base

---
+++ watch_datasql.sql
@@ -0,0 +1,39 @@
+create database watch_data;
+use watch_data;
+
+create table users (
+    id_users int(10) not null primary key auto_increment ,
+    nom varchar(50),
+    identifiant varchar(50),
+    mdp varchar(50)
+);
+
+create table categories(
+    id_categories int(10) not null primary key auto_increment ,
+    categories varchar(100),
+    images varchar(255),
+    featured varchar(10),
+    active varchar(10)
+);
+
+create table order(
+    id_order int(10) not null primary key auto_increment ,
+    id_produit int,
+    id_users int,
+    prix decimal,
+    qte int,
+    montant_total_produits decimal,
+    statut int default 1,
+    date_achat datetime,
+);
+
+create table produit(
+    id_produit int(10) not null primary key auto_increment,
+    produit varchar(50),
+    descriptions varchar(255),
+    prix decimal,
+    images varchar(255),
+    id_categories int(10),
+    featured varchar(10),
+    active varchar(10)
+);
